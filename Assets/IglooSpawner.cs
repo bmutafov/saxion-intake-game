f537a5d05d990efb24e95f004887a72510ddd1df
Boris Mutafov <mutafow@gmail.com>
2017-11-24 01:06:50 +0200

    Initial commit

---
+++ Assets/IglooSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IglooSpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject iglooPrefab;
+
+	private void Start ()
+	{
+		int iglooCount = Random.Range(10, 20);
+		for ( int i = 0 ; i < iglooCount ; i++ )
+		{
+			GameObject igloo = Instantiate(iglooPrefab, new Vector2(Random.Range(6, 55), Random.Range(6, 55)), Quaternion.identity);
+			igloo.tag = "igloo";
+		}
+	}
+}
